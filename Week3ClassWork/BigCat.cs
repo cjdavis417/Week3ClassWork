f5863390ac32ed8365dde60ca356c87011b8acc9
Chris Davis <christopherjdavis417@gmail.com>
2018-09-18 18:48:33 -0400

    Add project files.

---
+++ Week3ClassWork/BigCat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3ClassWork
+{
+    class BigCat
+    {
+        public string Type { get; set; }
+        public int Height { get; set; }
+        public int Length { get; set; }
+        public int Weight { get; set; }
+        public double Speed { get; set; }
+
+    }
+}
