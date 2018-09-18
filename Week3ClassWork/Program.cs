ab1d48dd34a8788a1844ae1f28971bc6cf40638e
Chris Davis <christopherjdavis417@gmail.com>
2018-09-18 19:08:54 -0400

    initial object creation to object type.

--- Week3ClassWork/Program.cs
+++ Week3ClassWork/Program.cs
@@ -10,6 +10,60 @@ namespace Week3ClassWork
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Big Cats v1.0 \n");
+            Console.Write("Would you like to create a new Cat? y/n ");
+
+            var initialResponse = Console.ReadLine();
+
+            if (initialResponse == "y")
+            {
+                var newCat = new BigCat();
+                Console.Write("What type of Cat is this? ");
+                newCat.Type = Console.ReadLine();
+                Console.WriteLine();
+                int intParse;
+                Console.Write("What is the Height of this cat? ");
+                if (int.TryParse(Console.ReadLine(), out intParse))
+                {
+                    newCat.Height = intParse;
+                }
+
+                Console.WriteLine();
+                Console.Write("What is the Length of this cat? ");
+                if (int.TryParse(Console.ReadLine(), out intParse))
+                {
+                    newCat.Length = intParse;
+                }
+
+                Console.WriteLine();
+                Console.Write("What is the adult weight of this cat? ");
+                if (int.TryParse(Console.ReadLine(), out intParse))
+                {
+                    newCat.Weight = intParse;
+                }
+
+                double doubleParse;
+                Console.WriteLine();
+                Console.Write("What is the Maximum running speed of this cat? ");
+                if (double.TryParse(Console.ReadLine(), out doubleParse))
+                {
+                    newCat.Speed = doubleParse;
+                }
+
+
+                Console.WriteLine("Type: " + newCat.Type);
+                Console.WriteLine("Height: " + newCat.Height);
+                Console.WriteLine("Length: " + newCat.Length);
+                Console.WriteLine("Weight: " + newCat.Weight);
+                Console.WriteLine("MAX Speed: " + newCat.Speed);
+
+                Console.ReadLine();
+
+            }
+
+
         }
+
+        
     }
 }
